28e5841215e2697f130648fba43d56fd5ad34311
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-03-12 07:43:45 +0700

    Create url dashboard.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, home='dashboard'),
 ]

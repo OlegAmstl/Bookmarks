408cf27c6b09732716f4a04270f4f85947aaf3f2
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-03-13 06:35:13 +0700

    Create "password change".

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView,PasswordChangeView, PasswordChangeDoneView
 
 from . import views
 
@@ -8,5 +8,9 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, home='dashboard'),
+    path('', views.dashboard, name='dashboard'),
+    path('password-change/', PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]

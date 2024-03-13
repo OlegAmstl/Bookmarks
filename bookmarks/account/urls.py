ee9024211bd3cd4d419a92169640059931fd3d5e
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-03-13 15:36:11 +0700

    Create "password reset" urls.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView,PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView, \
+                                   PasswordChangeView, \
+                                   PasswordChangeDoneView, \
+                                   PasswordResetView, PasswordResetDoneView, \
+                                   PasswordResetConfirmView, \
+                                   PasswordResetCompleteView
 
 from . import views
 
@@ -13,4 +18,14 @@ urlpatterns = [
          name='password_change'),
     path('password-change/done/', PasswordChangeDoneView.as_view(),
          name='password_change_done'),
+    path('password-reset/', PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset/<uid64>/<token>/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete')
+         
+
 ]

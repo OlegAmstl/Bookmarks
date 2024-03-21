9c09a1ad2968a93c62e8846b19c61d6935f8daa7
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-03-22 06:55:47 +0700

    Create model Profile.

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Расширенная модель пользователя.
+    """
+
+    user = models.OneToOneField(settings.AUTH_USER,
+                                on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(blank=True, upload_to='users/%Y%m%d')
+
+    def __str__(self):
+        return f'Профиль пользователя {self.user.username}'
